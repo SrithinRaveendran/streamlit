66cdfb3051a11f25e000b30bdf213455cd5abf3b
Srithin R <166797663+SrithinRaveendran@users.noreply.github.com>
2024-07-09 08:39:52 +0530

    Update streamlit_run.py

--- streamlit_run.py
+++ streamlit_run.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
